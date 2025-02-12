162b06fbae1b1ee089b149d586ff04dec7904a1a
Blog 1703 <125159652+blog1703@users.noreply.github.com>
2025-02-12 16:27:14 +0300

    Update update_m3u.sh

--- update_m3u.sh
+++ update_m3u.sh
@@ -6,6 +6,12 @@ M3U_SOURCE="https://iptvshared.ucoz.net/IPTV_SHARED.m3u"
 # Имя файла для сохранения
 M3U_FILE="playlist.m3u"
 
+# Запрещенные категории
+BLOCKED_KEYWORDS="Adult|18+|Взрослые|ИНФО"
+
+# Фильтрация запрещенных категорий
+grep -ivE "$BLOCKED_KEYWORDS" $TEMP_FILE > $DESTINATION_PATH
+
 # Скачиваем файл
 wget -O "$M3U_FILE" "$M3U_SOURCE"
 
